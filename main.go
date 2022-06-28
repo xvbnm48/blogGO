384ced328c9781a1dfab0783f3e35f2fe27ef61d
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-06-28 22:58:15 +0700

    refactory!

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
-	"github.com/xvbnm48/blogGO/app/model"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	// fmt.Println("Hello, World!")
-	model.Init()
+
+	dsn := "root:@tcp(127.0.0.1:3306)/blogGo?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("connection database error, error: ", err)
+	}
+
+	fmt.Println("connection database success")
+	router := gin.Default()
+	v1 := router.Group("/v1")
+	v1.GET("/hello", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	router.Run(":8080")
 }
